9482a83468593bec51f8093fd06fb57679895a7e
Andrew Wen <wenanguo110@163.com>
2017-10-13 21:50:44 +0800

    完成分页及restful

--- code/app/models.py
+++ code/app/models.py
@@ -1,5 +1,5 @@
 from . import db
-
+from flask_login import UserMixin, AnonymousUserMixin,url_for
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -21,3 +21,45 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    def to_json(self):
+        json_user = {
+            'id': self.id,
+            'username': self.username,
+            'password': self.password,
+            'sex': self.sex
+        }
+        return json_user
+
+    @staticmethod
+    def generate_fake(count=100):
+        from sqlalchemy.exc import IntegrityError
+        from random import seed
+        import forgery_py
+
+        seed()
+        for i in range(count):
+            u = User(
+                     username=forgery_py.internet.user_name(True),
+                     password=forgery_py.lorem_ipsum.word())
+
+            db.session.add(u)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
+
+
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
