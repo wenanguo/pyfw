d361bd1c9b5a464c97f93637065902c41b95987b
Andrew Wen <wenanguo110@163.com>
2017-10-16 17:19:37 +0800

    update

--- code/tests/c1/test_job.py
+++ code/tests/c1/test_job.py
@@ -3,17 +3,17 @@
 
 
 __author__ = 'Andrew Wen'
-
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-
-def my_job():
-    print('hello world')
-
-
-sched = BlockingScheduler()
-sched.add_job(my_job, 'interval', seconds=5)
-sched.start()
+#
+# from apscheduler.schedulers.blocking import BlockingScheduler
+#
+#
+# def my_job():
+#     print('hello world')
+#
+#
+# sched = BlockingScheduler()
+# sched.add_job(my_job, 'interval', seconds=5)
+# sched.start()
 
 
 if __name__ =='__main__':
