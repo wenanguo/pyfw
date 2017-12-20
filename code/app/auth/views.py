1e0a0becafaa147c80b883daed24ef9417ac7b83
Andrew Wen <wenanguo110@163.com>
2017-12-20 13:06:04 +0800

    加入jquery easy-ui

--- code/app/auth/views.py
+++ code/app/auth/views.py
@@ -97,6 +97,24 @@ def register():
 
 
 
+
+
+
+
+@auth.route('/userlist', methods=['GET', 'POST'])
+def userlist():
+    """
+    获取用户列表
+    :return:
+    """
+
+    return render_template('auth/users.html')
+
+
+
+
+
+
 @auth.route('/ytxtest', methods=['GET', 'POST'])
 def ytxtest():
     error = None
