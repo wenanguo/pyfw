497c538894b4c86ab1a25a9ff4471b01395daf87
Andrew Wen <wenanguo110@163.com>
2018-07-13 10:59:23 +0800

    update

--- code/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-' module description'
-
-__author__ = 'Andrew Wen'
-
-__version__="v1.0.2"
--- code/run.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-
-echo "===启动应用==="
-#python manage.py runserver --host 0.0.0.0
-
-gunicorn -b 0.0.0.0:8000 manage:app
